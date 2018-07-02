c69f47c4cba1354a0bc5eddc5f551b12780e9c60
Clint Monzon <clint@ativistar.com>
2018-07-02 14:06:26 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clint::Application.config.secret_token = 'ef282ee7e2a6387f02757f72afafcffce29a5c9bdbfdf9d942865fcf7ec450a957e5cff83b9a1eb3439c9fe03cc9e978055631453d0239849d13fbeb5834e54c'
